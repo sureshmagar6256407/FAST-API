0204e519c5b90c224249715fd85cab4284e8bcc8
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-08-30 15:00:58 +0545

    update

--- practice.py
+++ practice.py
@@ -100,12 +100,101 @@
 
 
 
+# from fastapi import FastAPI  
+# app = FastAPI()
+
+# @app.get("/employees/{employee_id}")
+# def show_employee(employee_id:int,department : str) : 
+#     return { 
+#         "employee_id" : employee_id , 
+#         "department":department
+#     }
+
+
+
+# from fastapi import FastAPI 
+# from pydantic import BaseModel
+# from typing import Optional
+# app  =FastAPI()
+
+# class StudentMangement(BaseModel) : 
+#     name :str  
+#     age : int 
+#     marks  :int  
+#     city:str  
+
+# students_obj = [
+#     {"student_id":1,"name":"ram","age":20,"city":"kathmandu","marks":75} ,
+#     {"student_id":2,"name":"sita","age":21,"city":"butwal","marks":85} ,
+    
+
+# ]
+
+
+# @app.get("/students")  
+# def show_student (city:Optional[str] =None) : 
+#     if city : 
+#         filtered_students = [s for s in students_obj if s["city"].lower()==city.lower()]
+#         return filtered_students
+#     return students_obj
+
+# @app.get("/students/{student_id}")
+# def get_student_by_id (student_id:int): 
+#     for student in students_obj : 
+#         if student["student_id"] ==student_id : 
+#             return student
+
+#     raise ValueError("student not found")
+
+
+# @app.post("/students")
+# def create_student(student:StudentMangement) : 
+#     new_id = len(students_obj) +1   
+
+#     student_data = student.model_dump()
+#     student_data["student_id"] = new_id  
+
+#     students_obj.append(student_data)
+#     return {"message" : "student added succesfully ","data":student_data}
+
+
+
+
+
+from itertools import product
 from fastapi import FastAPI  
+from pydantic import BaseModel
 app = FastAPI()
 
-@app.get("/employess/{employee_id}")
-def show_employee(employee_id:int,department : str) : 
-    return { 
-        "employee_id" : employee_id , 
-        "department":department
+class Product_details(BaseModel) : 
+    name :str  
+    category:str  
+    price:float  
+    stock :int
+
+available_product =  [ 
+    {"id":1, "name":"Samsung Mobile" , "category":"Mobile", "price":55550 , "stock":10} , 
+    {"id":2, "name":"Charger" , "category":"Electronic", "price":1000, "stock":50}, 
+    {"id":3, "name":"Fan" , "category":"Electronic", "price":10000 , "stock":33}
+
+]
+
+
+@app.get("/products")
+def get_products() : 
+    return  { 
+        "details" :available_product
     }
+    
+
+@app.get("/products/{product_id}")
+def path_para (product_id :int) : 
+    pass 
+
+@app.get("/products")
+def querry_para (category:int) : 
+    pass 
+
+@app.post("/products")
+def add_products () : 
+    pass
